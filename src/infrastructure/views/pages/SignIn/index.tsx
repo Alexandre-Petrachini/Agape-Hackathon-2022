27a060d0f80f5849d6d7e6f9628ddd51922c1dea
Matheus <matheussilva.dd@gmail.com>
2022-07-10 07:10:12 -0300

    feat: add login page

--- src/infrastructure/views/pages/SignIn/index.tsx
+++ src/infrastructure/views/pages/SignIn/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.scss";
+import Logo from "../../../../images/login_logo.png";
+import Input from "../../components/Input";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (value: string) => {
+    setUsername(value);
+  };
+
+  const handlePasswordChange = (value: string) => {
+    setPassword(value);
+  };
+
+  return (
+    <div id="signin-screen">
+      <img src={Logo} />
+
+      <form>
+        <Input
+          label="Usuário:"
+          error=""
+          id="signin-user"
+          onChange={handleUsernameChange}
+        />
+
+        <Input
+          label="Senha:"
+          error=""
+          id="signin-password"
+          isPassword
+          onChange={handlePasswordChange}
+        />
+
+        <button>Entrar</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
